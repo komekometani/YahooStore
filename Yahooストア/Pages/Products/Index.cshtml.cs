34a9c49da8c2c0f293054e80358ec5cb74293590
佳恭 米谷 <k.kometani@workup.jp>
2020-04-10 16:22:32 +0900

    APIコントローラー追加

--- Yahooストア/Pages/Products/Index.cshtml.cs
+++ Yahooストア/Pages/Products/Index.cshtml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Form;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
